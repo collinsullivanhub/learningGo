e7452659f7dd6d69beefead42bd6987ba4035b72
Collin Sullivan <cs@hordelacrosse.com>
2018-10-24 15:10:42 -0400

    Add files via upload

---
+++ gopractice3.go
@@ -0,0 +1,28 @@
+
+package main 
+
+import (
+"fmt"
+"time"
+"sync"
+)
+
+func main(){
+
+	var waitYO sync.WaitGroup
+	waitYO.Add(1)
+	go func() {
+		count("sheep")
+		waitYO.Done()
+	}()
+	waitYO.Wait()
+}
+
+func count(thing string) {
+	for i := 1; i <= 5; i++{
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+
